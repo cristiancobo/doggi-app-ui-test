5899b2fd763e0145b5bc0279930c207670402862
Mayumi Tamura <maythermyself@gmail.com>
2021-05-25 03:46:06 -0500

    e2e ui modified

--- cypress/integration/deleteDogBreed.spec.js
+++ cypress/integration/deleteDogBreed.spec.js
@@ -1,20 +1,41 @@
 const { random } = require('faker');
+const { describe } = require('mocha');
 const { BASE_URL, TIME_OUT } = require('../utils/constants');
 
 const new_dog_breed = {
-    
     name: random.words(2),
-    weight: random.
+    weight: random.number(50),
+    height: random.number(50),
+    lifeExpectancy: random.number(20),
 } 
 
-describe("", () => {
+describe("Given a created dog", () => {
 
     before(() => {
         cy.visit(BASE_URL);
+        cy.get(':nth-child(2) > .MuiButtonBase-root > .MuiButton-label').click();
         cy.wait(TIME_OUT);
-    })
+        cy.get('[data-testid=e2e-breed-dog-name-input]').type(new_dog_breed.name);
+        cy.wait(TIME_OUT);
+        cy.get('[data-testid=e2e-breed-dog-weight-input]').type(new_dog_breed.weight);
+        cy.wait(TIME_OUT);
+        cy.get('[data-testid=e2e-breed-dog-height-input]').type(new_dog_breed.height);
+        cy.wait(TIME_OUT);
+        cy.get('[data-testid=e2e-breed-dog-life-expectancy-input]').type(new_dog_breed.lifeExpectancy);
+        cy.wait(TIME_OUT);
+        cy.get('[data-testid=e2e-select-country-button]').click();
+        cy.get('.Mui-selected').click();
+        cy.wait(TIME_OUT);
+    });
 
-    it("", () => {
+    describe("When the user wants to delete the dog", () => {
+        
+        before(() => {
 
-    })
+        });
+
+        it("Then the dog should not be listed", () => {
+
+        });
+    });
 });
--- cypress/integration/listDogBreeds.spec.js
+++ cypress/integration/listDogBreeds.spec.js
@@ -1,20 +1,12 @@
-const { random } = require('faker');
 const { BASE_URL, TIME_OUT } = require('../utils/constants');
 
-const new_dog_breed = {
-    
-    name: random.words(2),
-    weight: random.
-} 
-
-describe("", () => {
+describe("When the user wants to list the existing dogs", () => {
 
     before(() => {
         cy.visit(BASE_URL);
-        cy.wait(TIME_OUT);
-    })
+    });
 
-    it("", () => {
-
+    it("Then there should be more than zero elements", () => {
+        
     })
 });
